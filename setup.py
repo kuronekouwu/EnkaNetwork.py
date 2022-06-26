25fe56c8f51935547abfc934f5fd46589352269d
M-307 <unturnedmc1@gmail.com>
2022-06-26 21:07:17 +0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
 	name="enkanetwork.py",
-	version="1.0.0",
+	version="1.1.0",
 	author="M-307",
 	author_email="contact@m-307.tk",
 	description="Library for fetching JSON data from site https://enka.shinshin.moe/",
-	long_description=open("./README.md", "r", encoding="utf-8").read(),
+	long_description=open("README.md", "r", encoding="utf-8").read(),
 	long_description_content_type="text/markdown",
 	url="https://github.com/mrwan200/enkanetwork-py",
 	keywords = ['enkanetwork.py', 'enkanetwork', 'enka.shinshin.moe', 'genshinapi'],
@@ -16,7 +16,10 @@ setuptools.setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	install_requires=open("./requirements.txt", "r", encoding="utf-8").readlines(),
+	install_requires=[
+		"pydantic",
+		"aiohttp"
+	],
 	python_requires=">=3.6",
 	include_package_data=True
 )
