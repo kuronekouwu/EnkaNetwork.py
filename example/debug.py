397f10b8bd35f37be463791cbc2a2cb8cf6b0585
M-307 <unturnedmc1@gmail.com>
2022-07-05 15:48:25 +0700

    add example for debugging

---
+++ example/debug.py
@@ -0,0 +1,12 @@
+import asyncio
+
+from enkanetwork import EnkaNetworkAPI
+
+client = EnkaNetworkAPI(lang="th", debug=True)
+
+async def main():
+    await client.fetch_user(843715177)
+    # You can see the debug log in console.
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
