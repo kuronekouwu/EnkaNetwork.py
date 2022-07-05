dca6f5de4a60749e324c9d40be4b5a496f5de12b
M-307 <unturnedmc1@gmail.com>
2022-07-05 13:39:17 +0700

    Update to support show name, icon, skills and more.

--- enkanetwork/model/character.py
+++ enkanetwork/model/character.py
@@ -1,42 +1,98 @@
-from pydantic import BaseModel, Field, validator
-from typing import List, Dict
-from enum import Enum
+from pydantic import BaseModel, Field
+from typing import List, Any
 
-from .artifact import Artifacts
+from .equipments import Equipments
 from .combats import CharacterCombat
-
-class CharacterPropertieType(int, Enum):
-    UNKNOW = -1
-    XP = 1001
-    ASENCSION = 1002
-    LEVEL = 4001
+from ..json import Config
+from ..utils import create_ui_path
 
 class ChatacterFriendshipLevel(BaseModel):
     level: int = Field(0, alias="expLevel")
 
-class CharacterPropertie(BaseModel):
-    type: CharacterPropertieType
-    ival: str = Field("", alias="ival")
-    value: str = Field("", alias="val")
+class CharacterImages(BaseModel):
+    icon: str = ""
+    side: str = ""
+    gacha: str = ""
 
-    @validator('type', pre=True)
-    def check_type(cls, val: int):
-        if val not in [1001,1002,4001]:
-            val = -1
+class CharacterSkill(BaseModel):
+    id: int = 0
+    name: str = ""
+    icon: str = ""
+    level: int = 0
 
-        return val
-
-    class Config:
-        use_enum_values = True
+class CharacterConstellations(BaseModel):
+    id: int = 0
+    name: str  = ""
+    icon: str = ""
+    unlocked: bool = False # If character has this constellation.
 
 class CharacterInfo(BaseModel):
+    """
+        API Response data
+    """
     id: int = Field(0, alias="avatarId")
     friendship: ChatacterFriendshipLevel = Field({},alias="fetterInfo")
-    propertie: Dict[str, CharacterPropertie] = Field({}, alias="propMap")
-    # Artifacts
-    artifacts: List[Artifacts] = Field([], alias="equipList")
+    equipments: List[Equipments] = Field([], alias="equipList")
     combat: CharacterCombat = Field({}, alias="fightPropMap")
     skill_data: List[int] = Field([], alias="inherentProudSkillList")
     skill_id: int = Field(0, alias="skillDepotId")
 
-    skill_level: Dict[str, int] = Field({}, alias="skillLevelMap")
+    """
+        Custom data
+    """
+    name: str = "" # Get from name hash map 
+    image: CharacterImages = CharacterImages()
+    skills: List[CharacterSkill] = []
+    constellations: List[CharacterConstellations] = []
+
+    # Prop Maps
+    xp: int = 0 # AKA. propMap 1001
+    ascension: int = 0 # AKA. propMap 4001
+    level: int = 0 # AKA. propMap 1002
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        super().__init__(**data)
+
+        # Get character
+        character = Config.DATA["characters"].get(str(data["avatarId"]))
+
+        # Check if character is 
+        if not character:
+            return
+        # Get name hash map
+        name = Config.HASH_MAP["characters"].get(str(character["NameTextMapHash"]))
+
+        # Get name from hash map and icon
+        __pydantic_self__.name = name[Config.LANGS]
+        __pydantic_self__.image = CharacterImages(
+            icon=create_ui_path(character["SideIconName"].replace("_Side", "")),
+            side=create_ui_path(character["SideIconName"]),
+            gacha=create_ui_path(character["SideIconName"].replace("AvatarIcon_Side", "Gacha_AvatarImg"))
+        )
+
+        # Get prop map
+        __pydantic_self__.xp = int(data["propMap"]["1001"]["ival"]) if "1001" in data["propMap"] else 0
+        __pydantic_self__.ascension = int(data["propMap"]["1002"]["ival"]) if "1002" in data["propMap"] else 0
+        __pydantic_self__.level = int(data["propMap"]["4001"]["ival"]) if "4001" in data["propMap"] else 0
+
+        # Load constellation
+        for constellation in character["Consts"]:
+            _constellation = Config.DATA["constellations"].get(str(constellation))
+            if _constellation:
+                __pydantic_self__.constellations.append(CharacterConstellations(
+                    id=_constellation["talentId"],
+                    name=Config.HASH_MAP["constellations"].get(str(_constellation["nameTextMapHash"]))[Config.LANGS],
+                    icon=create_ui_path(constellation),
+                    unlocked=_constellation["talentId"] in data["talentIdList"] if "talentIdList" in data else False
+                ))
+        
+        # Load skills 
+        for skill in character["SkillOrder"]:
+            _skill = Config.DATA["skills"].get(str(skill))
+            if _skill:
+                __pydantic_self__.skills.append(CharacterSkill(
+                    id=skill,
+                    name=Config.HASH_MAP["skills"].get(str(_skill["nameTextMapHash"]))[Config.LANGS],
+                    icon=create_ui_path(_skill["skillIcon"]),
+                    level=data["skillLevelMap"].get(str(skill), 0)
+                ))
--- enkanetwork/model/players.py
+++ enkanetwork/model/players.py
@@ -1,24 +1,98 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Any
+
+from ..json import Config
+from ..utils import create_ui_path
 
 class ProfilePicture(BaseModel):
-    avatarId: int
+    """
+        API Response data
+    """
+    id: int = Field(0, alias="avatarId")
+
+    """
+        Custom add data
+    """
+    icon: str = ""
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        super().__init__(**data)
+
+        # Get character
+        character = Config.DATA["characters"].get(str(data["avatarId"]))
+        if character:
+            __pydantic_self__.icon = create_ui_path(character["SideIconName"].replace("_Side", ""))
 
 class showAvatar(BaseModel):
-    avatarId: str
-    level: int
+    """
+        API Response data
+    """
+    id: str = Field(0, alias="avatarId")
+    level: int = 1
+
+    """
+        Custom data
+    """
+    name: str = ""
+    icon:  str = ""
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        super().__init__(**data)
+
+        # Get character
+        character = Config.DATA["characters"].get(str(data["avatarId"]))
+        if character:
+            # Get name hash map
+            name = Config.HASH_MAP["characters"].get(str(character["NameTextMapHash"]))
+
+            __pydantic_self__.name = name[Config.LANGS]
+            __pydantic_self__.icon = create_ui_path(character["SideIconName"].replace("_Side", ""))
+
+class Namecard(BaseModel):
+    id: int = 0
+    icon: str = ""
+    banner: str = ""
+    navbar: str = ""
+    name: str = ""
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        super().__init__(**data)
+
+        if __pydantic_self__.id > 0:
+            # Get name card
+            namecard = Config.DATA["namecards"].get(str(__pydantic_self__.id))
+
+            if namecard:
+                __pydantic_self__.name = Config.HASH_MAP["namecards"].get(str(namecard["nameTextMapHash"]))[Config.LANGS]
+                __pydantic_self__.icon = create_ui_path(namecard["icon"])
+                __pydantic_self__.banner = create_ui_path(namecard["banner"])
+                __pydantic_self__.navbar = create_ui_path(namecard["navbar"])
 
 class PlayerInfo(BaseModel):
+    """
+        API Response data
+    """
     # Profile info
-    finishAchievementNum: int
-    level: int
-    nameCardId: int
-    nickname: str
-    signature: str
-    worldLevel: int
-    profilePicture: ProfilePicture
+    achievement: int = Field(0, alias="finishAchievementNum")
+    level: int = 0
+    nickname: str = ""
+    signature: str = ""
+    world_level: int = Field(1, alias="worldLevel")
+    profile_picture: ProfilePicture = Field(None, alias="profilePicture")
     # Avatars
-    showAvatarInfoList: List[showAvatar]
+    characters_preview: List[showAvatar] = Field([], alias="showAvatarInfoList")
     # Abyss floor
-    towerFloorIndex: int
-    towerLevelIndex: int
+    abyss_floor: int = Field(0, alias="towerFloorIndex")
+    abyss_room: int = Field(0, alias="towerLevelIndex")
+
+    """
+        Custom data
+    """
+    namecard: Namecard = Namecard() # Profile namecard
+    list_namecard: List[Namecard] = [] # List namecard preview in profile
+
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        super().__init__(**data)
+        
+        __pydantic_self__.namecard = Namecard(id=data["nameCardId"])
+        __pydantic_self__.list_namecard = [Namecard(id=namecard) for namecard in data["showNameCardIdList"]]
