b6da8595415fc26165e22611dafda18ab1d903d8
M-307 <unturnedmc1@gmail.com>
2022-07-05 16:26:03 +0700

    Fix bug

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
 	name="enkanetwork.py",
-	version="1.2.0",
+	version="1.2.1",
 	author="M-307",
 	author_email="me@m-307.tk",
 	description="Library for fetching JSON data from site https://enka.shinshin.moe/",
 	long_description=open("README.md", "r", encoding="utf-8").read(),
 	long_description_content_type="text/markdown",
-	url="https://github.com/mrwan200/enkanetwork-py",
+	url="https://github.com/mrwan200/EnkaNetwork.py",
 	keywords = ['enkanetwork.py', 'enkanetwork', 'enka.shinshin.moe', 'genshinapi'],
 	packages=setuptools.find_packages(),
 	classifiers=[
