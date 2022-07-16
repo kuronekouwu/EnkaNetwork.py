b96cce9f5be31926227c16c60a7aa4a2dc43493b
M-307 <unturnedmc1@gmail.com>
2022-07-17 02:37:05 +0700

    Add example assets data

---
+++ example/assets/character.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from enkanetwork import Assets
+
+assets = Assets(lang="th")
+
+async def main():
+    character = assets.character(10000046)
+    name = assets.get_hash_map(character.hash_id)
+
+    print("=== Asset character ===")
+    print(f"ID: {character.id}")
+    print(f"Name (Hash ID): {character.hash_id}")
+    print(f"Name (Hashed): {name}")
+    print(f"Icon URL: {character.images.icon}")
+    print(f"Side Icon URL: {character.images.side}")
+    print(f"Banner URL: {character.images.banner}")
+    print(f"=== Skills ===")
+    for skill in character.skills:
+        skill_info = assets.skills(skill)
+        name = assets.get_hash_map(skill_info.hash_id)
+        print(f"ID: {skill_info.id}")
+        print(f"Name (Hash ID): {skill_info.hash_id}")
+        print(f"Name (Hashed): {name}")
+        print(f"Icon URL: {skill_info.icon}")
+    
+    print(f"=== Constellations ===")
+    for constellation in character.constellations:
+        constellation_info = assets.constellations(constellation)
+        name = assets.get_hash_map(constellation_info.hash_id)
+        print(f"ID: {constellation_info.id}")
+        print(f"Name (Hash ID): {constellation_info.hash_id}")
+        print(f"Name (Hashed): {name}")
+        print(f"Icon URL: {constellation_info.icon}")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
---
+++ example/assets/namecard.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from enkanetwork import Assets
+
+assets = Assets(lang="th")
+
+async def main():
+    namecard = assets.namecards(210059)
+    name = assets.get_hash_map(namecard.hash_id)
+
+    print("=== Asset character ===")
+    print(f"ID: {namecard.id}")
+    print(f"Name (Hash ID): {namecard.hash_id}")
+    print(f"Name (Hashed): {name}")
+    print(f"Icon: {namecard.icon}")
+    print(f"Banner URL: {namecard.banner}")
+    print(f"Navbar (Alpha) URL: {namecard.navbar}")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
