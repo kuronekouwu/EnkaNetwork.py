2cb119748405d6995bb47012fea1a44b6cc5cee4
M-307 <unturnedmc1@gmail.com>
2022-07-17 02:35:08 +0700

    Change Config to Assets data

--- enkanetwork/client.py
+++ enkanetwork/client.py
@@ -6,7 +6,7 @@ from typing import Union
 
 from .model import EnkaNetworkResponse
 from .exception import VaildateUIDError, UIDNotFounded
-from .json import Config
+from .assets import Assets
 from .utils import create_path, validate_uid, request
 
 class EnkaNetworkAPI:
@@ -21,16 +21,10 @@ class EnkaNetworkAPI:
         logging.getLogger("enkanetwork").setLevel(logging.DEBUG if debug else logging.ERROR)
 
         # Set language and load config
-        self.__load_config(lang)
+        self.assets = Assets(lang)
 
         # Key
         self.__key = key
-    
-    def __load_config(self, lang) -> None:
-        Config.set_languege(lang)
-
-        Config.load_json_data()
-        Config.load_json_lang()
 
     async def fetch_user(self, uid: Union[str, int]) -> EnkaNetworkResponse:
         self.LOGGER.debug(f"Validating with UID {uid}...")
@@ -60,7 +54,7 @@ class EnkaNetworkAPI:
 
     async def download_data(self) -> None:
         self.LOGGER.debug("Downloading new content...")
-        _PATH = Config.get_path_json()
+        _PATH = Assets.get_path_json()
         for folder in _PATH:
             for filename in os.listdir(_PATH[folder]):
                 self.LOGGER.debug(f"Downloading {folder} file {filename}...")
@@ -73,4 +67,4 @@ class EnkaNetworkAPI:
                     json.dump(_json["content"], f, ensure_ascii=False, indent=4)
 
         # Load config 
-        self.__load_config(Config.LANGS)
+        self.__load_config(Assets.LANGS)
