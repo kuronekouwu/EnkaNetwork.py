1db0e9e25b93ae8c695b15f76c2bedccb47f53e9
M-307 <unturnedmc1@gmail.com>
2022-07-17 13:08:59 +0700

    Fix wrong example

--- example/skills.py
+++ example/skills.py
@@ -8,11 +8,11 @@ async def main():
     data = await client.fetch_user(843715177)
     for character in data.characters:
         print(f"=== Skill of {character.name} ===")
-        for constellation in character.constellations:
-            print(f"ID: {constellation.id}")
-            print(f"Name: {constellation.name}")
-            print(f"Icon: {constellation.icon}")
-            print(f"Unlocked: {constellation.unlocked}")
+        for skill in character.skills:
+            print(f"ID: {skill.id}")
+            print(f"Name: {skill.name}")
+            print(f"Icon: {skill.icon}")
+            print(f"Level: {skill.level}")
             print("="*18)
 
 loop = asyncio.get_event_loop()
