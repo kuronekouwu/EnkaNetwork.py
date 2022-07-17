9e7a3cfa068bacb5c0ca1e90d325a9ccea17edd1
M-307 <unturnedmc1@gmail.com>
2022-07-17 13:06:22 +0700

    Change filename "download_data" to "update_assets"

---
+++ example/update_assets.py
@@ -0,0 +1,12 @@
+import asyncio
+
+from enkanetwork import EnkaNetworkAPI
+
+client = EnkaNetworkAPI(debug=True)
+
+async def main():
+    await client.update_assets()
+    # You can see the progress download new assets in console
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
