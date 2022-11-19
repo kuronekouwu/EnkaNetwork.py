324c5648c8c9f123b456752c6c5a659058111947
M307 (Mac) <me@m307.dev>
2022-11-19 16:52:33 +0700

    :barbaragun:

--- enkanetwork/model/__init__.py
+++ enkanetwork/model/__init__.py
@@ -1,8 +1,6 @@
-from pydantic import BaseModel, Field
-from typing import List
-
 from .players import *
 from .character import *
 from .equipments import *
 from .players import *
 from .stats import *
+from .base import *
