a8aeebc325c08399bae1ef5e1213f9072bb822b3
M307 (Mac) <me@m307.dev>
2022-11-19 18:13:15 +0700

    Move back into base

--- enkanetwork/model/__init__.py
+++ enkanetwork/model/__init__.py
@@ -1,14 +1,6 @@
-from pydantic import BaseModel, Field
-from typing import List
-
 from .players import *
 from .character import *
 from .equipments import *
 from .players import *
 from .stats import *
-# from .base import *
-
-class EnkaNetworkResponse(BaseModel):
-    player: PlayerInfo = Field(None, alias="playerInfo")
-    characters: List[CharacterInfo] = Field(None, alias="avatarInfoList")
-    ttl: int = 0
+from .base import *
