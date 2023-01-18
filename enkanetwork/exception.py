55c2ec468ee3ac4238da9f49ae03ee9f7dad3cd9
M307 (Mac) <me@m307.dev>
2023-01-18 20:05:08 +0700

    Remove pass

--- enkanetwork/exception.py
+++ enkanetwork/exception.py
@@ -1,22 +1,17 @@
 class VaildateUIDError(Exception):
     """ Raised when the UID is not valid. """
-    pass
 
 class UIDNotFounded(Exception):
     """ Raised when the UID is not found. """
-    pass
 
 class HTTPException(Exception):
     """ Exception that's raised when an HTTP request operation fails. """
-    pass
 
 class Forbidden(HTTPException):
     """ Exception that's raised for when status code 403 occurs."""
-    pass
 
 class EnkaServerError(HTTPException):
     """ Exception that's raised for when status code 500 occurs."""
-    pass
 
 class EnkaServerMaintanance(HTTPException):
     """ Exception that's raised when status code 424 occurs. """
