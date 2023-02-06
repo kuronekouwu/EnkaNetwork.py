5b344f609ff2bb2637e7490c6f5fb2a7b5bf2522
M307 (Mac) <me@m307.dev>
2023-02-06 22:04:59 +0700

    Update exception - Add new error or Enka error status code - Add new error exception - Depercated key field - Add new route

--- enkanetwork/exception.py
+++ enkanetwork/exception.py
@@ -1,17 +1,38 @@
 class VaildateUIDError(Exception):
     """ Raised when the UID is not valid. """
 
-class UIDNotFounded(Exception):
-    """ Raised when the UID is not found. """
+class ProfileNotFounded(Exception):
+    """ Raised when the profile is not found. """
+
+class BuildNotPublicData(Exception):
+    """ Raised when the profile hoyos has public to hidden """
 
 class HTTPException(Exception):
     """ Exception that's raised when an HTTP request operation fails. """
 
-class Forbidden(HTTPException):
-    """ Exception that's raised for when status code 403 occurs."""
+class EnkaValidateFailed(HTTPException):
+    """ Exception that's raised for when status code 400 occurs."""
+
+class EnkaUIDNotFound(Exception):
+    """ Raised when the UID is not found. """
 
 class EnkaServerError(HTTPException):
     """ Exception that's raised for when status code 500 occurs."""
 
 class EnkaServerMaintanance(HTTPException):
     """ Exception that's raised when status code 424 occurs. """
+
+class EnkaServerRateLimit(HTTPException):
+    """ Exception that's raised when status code 429 occurs."""
+
+class EnkaServerUnknown(HTTPException):
+    """ Exception that's raised when status code 503 occurs. """
+
+ERROR_ENKA = {
+    400: [VaildateUIDError, "Validate UID {uid} failed."],
+    404: [EnkaUIDNotFound, "UID {uid} not found. Please check your UID"],
+    429: [EnkaServerRateLimit, "Enka.network has been rate limit this path"],
+    424: [EnkaServerMaintanance, "Enka.Network doing maintenance server. Please wait took 5-8 hours or 1 day"],
+    500: [EnkaServerError, "Enka.network server has down or Genshin server broken."],
+    503: [EnkaServerUnknown, "I screwed up massively"]
+}
