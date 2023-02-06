0a45919f222c3ee623441c228875fa2ad058dad7
M307 (Mac) <me@m307.dev>
2023-02-06 22:05:21 +0700

    Add config.py and move some variable to config

---
+++ enkanetwork/config.py
@@ -0,0 +1,31 @@
+from typing import (
+    ClassVar
+)
+from .utils import get_user_agent
+from .cache import Cache
+
+class Config:
+    # HTTP Config
+    ENKA_PROTOCOL: ClassVar[str] = "https"
+    ENKA_URL: ClassVar[str] = "dev.enka.network"
+    # Assets
+    ASSETS_PROTOCOL: ClassVar[str] = "https"
+    ASSETS_URL: ClassVar[str] = "raw.githubusercontent.com"
+    # Header Config
+    USER_AGENT: ClassVar[str] = get_user_agent()
+    # Client config
+    CACHE_ENABLED: ClassVar[bool] = True
+    CACHE: ClassVar[Cache] = Cache(1024, 60 * 3)
+
+    @classmethod
+    def init_cache(
+        cls, 
+        cache: Cache,
+        enabled: bool = True
+    ):
+        cls.CACHE = cache
+        cls.CACHE_ENABLED = enabled
+    
+    @classmethod
+    def init_user_agent(cls, agent: str):
+        cls.USER_AGENT = agent
